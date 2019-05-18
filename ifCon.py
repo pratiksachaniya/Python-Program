7726dfb1ef674c7d545667cadc59833cf9baba12
Pratik Sachaniya <pratiksachaniya@users.noreply.github.com>
2019-05-18 16:36:25 +0530

    new

---
+++ ifCon.py
@@ -0,0 +1,6 @@
+print("Enter Your Age:")
+age = int(input())
+if age >= 18:
+    print("You Are Adult")
+else:
+    print("You Are Not Adult")
---
+++ input.py
@@ -0,0 +1,9 @@
+print("Enter Name:")
+n = input()
+print("Hello " + n)
+"""
+#Type Casting
+Int = int()
+Float = float()
+Sting = str()
+"""
