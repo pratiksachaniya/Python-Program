4b37fd24eedb27bc3058501188a360c6b52a86a7
Pratik Sachaniya <34182240+pratiksachaniya@users.noreply.github.com>
2019-06-25 08:58:55 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,14 @@
+str = "HI Welcome To Py."
+print(str)
+str = 20;
+print(str)
+str = 'This is Secound String'
+print(str)
+str = "Hi 'Pratik'"
+print(str)
+str = """ Hi "Pratik 'Sachaniya'" """
+print(str)
+
+print("Str[9:]")
+print(str[9:])
+print(str[1:5])
